6d66027788bb4a02a16bf0305611daf43ef212b4
Clifford Dle Arnejo <juliana.young.balingasa07112001@gmail.com>
2023-05-11 21:14:18 +0800

    Landing page students

---
+++ home-smrms/index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" href="./assets/favcon.png"/>
+    <title>SMRMS | Quezon City University</title>
+    <link rel="stylesheet" href="index.css">
+</head>
+<body>
+    <div class="header">
+          <div class="header-name">
+              <img src="./assets/QCULogo.png" alt="">
+              <h3>Quezon City University</h3>
+          </div>
+
+          <div id="icon">
+            <a href="../student/redo/student-login.php"><button class="sigin">SIGIN</button></a>
+          </div>
+      </div>
+
+      <div class="container">
+          <div class="content-1">
+              <div class="con-text">
+                  <div class="text-content">
+                    <p><span class="title-text">Medical and Dental Services</span><br> <span style="font-size: 22px;">The SCHOOL CLINIC is responsible for safe-guarding the health of the students and employees of Quezon City University.</span></p>
+                    <a href="../student/redo/register.php"><button>Register Here</button></a>
+                  </div>
+              </div>
+              <div class="con-img">
+                <img src="./assets/QCUClinicLogo.png" alt="" style="
+                    margin: auto;
+  display: block;padding: 80px 0;
+                    width: 45%;">
+              </div>
+          </div>
+
+          <div class="content-2">
+              <div class="con-img">
+              <img src="./assets/qcupic.png" alt="" style="
+                    margin: auto;
+  display: block;padding: 25px 0;
+                    width: 75%;">
+              </div>
+
+              <div class="con-text">
+                  <div class="text-content">
+                    <p><span class="title-text">SCHEDULE</span><br><br><b><span style="font-size: 22px;"> <i class="fas fa-clinic-medical"></i>
+                    🔘 Medical Clinic 7:00 am to 7:00 pm</span><br><span style="font-size: 22px;"> <i class="fas fa-tooth"></i>
+                    🔘 Dental Clinic 7:00 am to 7:00 pm</span></p>
+                    <br> <br>
+                    <p style="font-size: 24px;"> <em> Monday to Friday except Saturdays, Sundays and Holidays </b></em> </p>
+
+                  </div>
+              </div>
+          </div>
+      </div>
+
+      <footer>
+
+      </footer>
+</body>
+</html>
